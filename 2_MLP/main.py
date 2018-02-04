1e3340cd3077db19320f2c0d27c28c0693da81ff
jjahanip <jahandar.jahani@gmail.com>
2018-02-03 20:41:28 -0600

    change conv from 2 to 3

--- 2_MLP/main.py
+++ 2_MLP/main.py
@@ -7,27 +7,41 @@ import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(mnist.train.labels)))
+print("- Test-set:\t\t{}".format(len(mnist.test.labels)))
+print("- Validation-set:\t{}".format(len(mnist.validation.labels)))
+
+
 # hyper-parameters
-learning_rate = 0.001
-epochs = 10
-batch_size = 128
-display_step = 100
+learning_rate = 0.001   # The optimization learning rate
+epochs = 10             # Total number of training epochs
+batch_size = 100        # Training batch size
+display_freq = 100      # Frequency of displaying the training results
 
 # Network Parameters
-d_input = 784  # MNIST data input dimension (img shape: 28*28)
-h1 = 200  # number of units in hidden layer
-n_classes = 10  # MNIST total classes (0-9 digits)
+# We know that MNIST images are 28 pixels in each dimension.
+img_h = img_w = 28
+
+# Images are stored in one-dimensional arrays of this length.
+img_size_flat = img_h * img_w
+
+# Number of classes, one class for each of 10 digits.
+n_classes = 10
+
+# number of units in the first hidden layer
+h1 = 200
 
 
 # weight and bais wrappers
 def weight_variable(name, shape):
     """
     Create a weight variable with appropriate initialization
-    name: weight name
-    shape: weight shape
-    
-    return: initialized weight variable
+    :param name: weight name
+    :param shape: weight shape
+    :return: initialized weight variable
     """
+
     initer = tf.truncated_normal_initializer(stddev=0.01)
     return tf.get_variable('W_' + name,
                            dtype=tf.float32,
@@ -38,10 +52,9 @@ def weight_variable(name, shape):
 def bias_variable(name, shape):
     """
     Create a bias variable with appropriate initialization
-    name: bias variable name
-    shape: bias variable shape
-    
-    return: initialized bias variable
+    :param name: bias variable name
+    :param shape: bias variable shape
+    :return: initialized bias variable
     """
     initial = tf.constant(0., shape=shape, dtype=tf.float32)
     return tf.get_variable('b_' + name,
@@ -51,7 +64,7 @@ def bias_variable(name, shape):
 
 def fc_layer(x, num_nodes, name, use_relu=True):
     """
-    Creates a fully-connected layer
+    Create a fully-connected layer
     :param x: input from previous layer
     :param num_nodes: number of hidden units in the fully-connected layer
     :param name: layer name
@@ -66,13 +79,12 @@ def fc_layer(x, num_nodes, name, use_relu=True):
     layer += b
     if use_relu:
         layer = tf.nn.relu(layer)
-
     return layer
 
 
 # Create graph
 # Placeholders for inputs (x), outputs(y)
-x = tf.placeholder(tf.float32, shape=[None, d_input], name='X')
+x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X')
 y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
 fc1 = fc_layer(x, h1, 'FC1', use_relu=True)
 output_logits = fc_layer(fc1, n_classes, 'OUT', use_relu=False)
@@ -99,14 +111,13 @@ with tf.Session() as sess:
             batch_x, batch_y = mnist.train.next_batch(batch_size)
 
             # Run optimization op (backprop)
-            _, loss_batch, acc = sess.run([optimizer, loss, accuracy],
-                                          feed_dict={x: batch_x, y: batch_y})
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
 
-            if iter % display_step == 0:
+            if iter % display_freq == 0:
                 # Calculate batch loss and accuracy
                 loss_batch, acc_batch = sess.run([loss, accuracy],
                                                  feed_dict={x: batch_x, y: batch_y})
-                print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.2f}".
+                print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.01%}".
                       format(iter, loss_batch, acc_batch))
 
         # validation
@@ -129,7 +140,7 @@ with tf.Session() as sess:
     fig = plt.figure()
     for i in range(n_images):
         ax = fig.add_subplot(2, 2, i + 1)
-        ax.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray')
+        ax.imshow(np.reshape(test_images[i], [img_h, img_w]), cmap='gray')
         ax.set_title("Model prediction: {}".format(preds[i]))
         ax.axis('off')
     plt.show()
