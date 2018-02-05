950323acf94607c904f82a72793fe867fbe52303
Mobiny <amobiny@hula-11.ee.e.uh.edu>
2018-02-05 15:09:42 -0600

    main file modified

--- 2_MLP/main.py
+++ 2_MLP/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 print("Size of:")
@@ -12,12 +13,11 @@ print("- Training-set:\t\t{}".format(len(mnist.train.labels)))
 print("- Test-set:\t\t{}".format(len(mnist.test.labels)))
 print("- Validation-set:\t{}".format(len(mnist.validation.labels)))
 
-
 # hyper-parameters
-learning_rate = 0.001   # The optimization learning rate
-epochs = 10             # Total number of training epochs
-batch_size = 100        # Training batch size
-display_freq = 100      # Frequency of displaying the training results
+learning_rate = 0.001  # The optimization learning rate
+epochs = 10  # Total number of training epochs
+batch_size = 100  # Training batch size
+display_freq = 100  # Frequency of displaying the training results
 
 # Network Parameters
 # We know that MNIST images are 28 pixels in each dimension.
@@ -89,14 +89,14 @@ y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
 fc1 = fc_layer(x, h1, 'FC1', use_relu=True)
 output_logits = fc_layer(fc1, n_classes, 'OUT', use_relu=False)
 
-# Define loss, optimizer, accuracy for training
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output_logits))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-correct_prediction = tf.equal(tf.argmax(output_logits, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# Define the loss function, optimizer, and accuracy
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output_logits), name='loss')
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name='Adam-op').minimize(loss)
+correct_prediction = tf.equal(tf.argmax(output_logits, 1), tf.argmax(y, 1), name='correct_pred')
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
-# predict classes for testing and evluation
-predicted_classes = tf.argmax(output_logits, axis=1)
+# Network predictions
+y_pred_cls = tf.argmax(output_logits, axis=1, name='predictions')
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -107,35 +107,35 @@ with tf.Session() as sess:
     num_tr_iter = int(mnist.train.num_examples / batch_size)
     for epoch in range(epochs):
         print('Training epoch: {}'.format(epoch))
-        for iter in range(num_tr_iter):
+        for iteration in range(num_tr_iter):
             batch_x, batch_y = mnist.train.next_batch(batch_size)
 
             # Run optimization op (backprop)
-            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+            feed_dict_batch = {x: batch_x, y: batch_y}
+            sess.run(optimizer, feed_dict=feed_dict_batch)
 
-            if iter % display_freq == 0:
-                # Calculate batch loss and accuracy
+            if iteration % display_freq == 0:
+                # Calculate and display the batch loss and accuracy
                 loss_batch, acc_batch = sess.run([loss, accuracy],
-                                                 feed_dict={x: batch_x, y: batch_y})
+                                                 feed_dict=feed_dict_batch)
                 print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.01%}".
-                      format(iter, loss_batch, acc_batch))
+                      format(iteration, loss_batch, acc_batch))
 
-        # validation
-        loss_valid, acc_valid = sess.run([loss, accuracy],
-                                         feed_dict={x: mnist.validation.images,
-                                                    y: mnist.validation.labels})
-        print("Validation Accuracy:",
-              sess.run(accuracy, feed_dict={x: mnist.validation.images,
-                                            y: mnist.validation.labels}))
-    # test the network
-    # Calculate accuracy
-    print("Testing Accuracy:",
-          sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                        y: mnist.test.labels}))
+        # Run validation after every epoch
+        feed_dict_valid = {x: mnist.validation.images, y: mnist.validation.labels}
+        loss_valid, acc_valid = sess.run([loss, accuracy], feed_dict=feed_dict_valid)
+        print('---------------------------------------------------------')
+        print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
+              format(epoch + 1, loss_valid, acc_valid))
+        print('---------------------------------------------------------')
+    # Test the network at the end of training
+    feed_dict_test = {x: mnist.test.images, y: mnist.test.labels}
+    loss_test, acc_test = sess.run([loss, accuracy], feed_dict=feed_dict_test)
+    print("Test loss: {0:.2f}, test accuracy: {1:.01%}".format(loss_test, acc_test))
     # Predict single images
     n_images = 4
     test_images = mnist.test.images[:n_images]
-    preds = sess.run(predicted_classes, feed_dict={x: test_images})
+    preds = sess.run(y_pred_cls, feed_dict={x: test_images})
     # Display
     fig = plt.figure()
     for i in range(n_images):
@@ -143,4 +143,58 @@ with tf.Session() as sess:
         ax.imshow(np.reshape(test_images[i], [img_h, img_w]), cmap='gray')
         ax.set_title("Model prediction: {}".format(preds[i]))
         ax.axis('off')
+    plt.show()
+
+
+def plot_example_errors():
+    # Use TensorFlow to get a list of boolean values
+    # whether each test-image has been correctly classified,
+    # and a list for the predicted class of each image.
+    correct, cls_pred = sess.run([correct_prediction, y_pred_cls],
+                                 feed_dict=feed_dict_test)
+
+    # Negate the boolean array.
+    incorrect = (correct == False)
+
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images = mnist.test.images[incorrect]
+
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+
+    # Get the true classes for those images.
+    cls_true = mnist.test.cls[incorrect]
+
+    # Plot the first 9 images.
+    plot_images(images=images[0:9],
+                cls_true=cls_true[0:9],
+                cls_pred=cls_pred[0:9])
+
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 9
+
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i].reshape((img_h, img_h)), cmap='binary')
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        ax.set_xlabel(xlabel)
+
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
     plt.show()
