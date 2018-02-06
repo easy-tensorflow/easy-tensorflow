d1fb4ac45f287dc18023c1572f7b143632158c6d
Aryan <amobiny@hula-lt03.ee.e.uh.edu>
2018-02-06 01:45:51 -0600

    finalized network

--- 2_MLP/main.py
+++ 2_MLP/main.py
@@ -1,7 +1,8 @@
 # imports
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
+from ops import fc_layer
+from utils import plot_example_errors
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -32,56 +33,6 @@ n_classes = 10
 # number of units in the first hidden layer
 h1 = 200
 
-
-# weight and bais wrappers
-def weight_variable(name, shape):
-    """
-    Create a weight variable with appropriate initialization
-    :param name: weight name
-    :param shape: weight shape
-    :return: initialized weight variable
-    """
-
-    initer = tf.truncated_normal_initializer(stddev=0.01)
-    return tf.get_variable('W_' + name,
-                           dtype=tf.float32,
-                           shape=shape,
-                           initializer=initer)
-
-
-def bias_variable(name, shape):
-    """
-    Create a bias variable with appropriate initialization
-    :param name: bias variable name
-    :param shape: bias variable shape
-    :return: initialized bias variable
-    """
-    initial = tf.constant(0., shape=shape, dtype=tf.float32)
-    return tf.get_variable('b_' + name,
-                           dtype=tf.float32,
-                           initializer=initial)
-
-
-def fc_layer(x, num_nodes, name, use_relu=True):
-    """
-    Create a fully-connected layer
-    :param x: input from previous layer
-    :param num_nodes: number of hidden units in the fully-connected layer
-    :param name: layer name
-    :param use_relu: boolean to add ReLU non-linearity (or not)
-    
-    :return: The output array
-    """
-    in_dim = x.get_shape()[1]
-    W = weight_variable(name, shape=[in_dim, num_nodes])
-    b = bias_variable(name, [num_nodes])
-    layer = tf.matmul(x, W)
-    layer += b
-    if use_relu:
-        layer = tf.nn.relu(layer)
-    return layer
-
-
 # Create graph
 # Placeholders for inputs (x), outputs(y)
 x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X')
@@ -96,7 +47,7 @@ correct_prediction = tf.equal(tf.argmax(output_logits, 1), tf.argmax(y, 1), name
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
 # Network predictions
-y_pred_cls = tf.argmax(output_logits, axis=1, name='predictions')
+cls_prediction = tf.argmax(output_logits, axis=1, name='predictions')
 
 # Initializing the variables
 init = tf.global_variables_initializer()
@@ -128,73 +79,14 @@ with tf.Session() as sess:
         print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
               format(epoch + 1, loss_valid, acc_valid))
         print('---------------------------------------------------------')
-    # Test the network at the end of training
+
+    # Test the network after training
     feed_dict_test = {x: mnist.test.images, y: mnist.test.labels}
     loss_test, acc_test = sess.run([loss, accuracy], feed_dict=feed_dict_test)
     print("Test loss: {0:.2f}, test accuracy: {1:.01%}".format(loss_test, acc_test))
-    # Predict single images
-    n_images = 4
-    test_images = mnist.test.images[:n_images]
-    preds = sess.run(y_pred_cls, feed_dict={x: test_images})
-    # Display
-    fig = plt.figure()
-    for i in range(n_images):
-        ax = fig.add_subplot(2, 2, i + 1)
-        ax.imshow(np.reshape(test_images[i], [img_h, img_w]), cmap='gray')
-        ax.set_title("Model prediction: {}".format(preds[i]))
-        ax.axis('off')
-    plt.show()
 
+    # Plot some of the misclassified examples
+    cls_pred = sess.run(cls_prediction, feed_dict=feed_dict_test)
+    cls_true = np.argmax(mnist.test.labels, axis=1)
+    plot_example_errors(mnist.test.images, cls_true, cls_pred)
 
-def plot_example_errors():
-    # Use TensorFlow to get a list of boolean values
-    # whether each test-image has been correctly classified,
-    # and a list for the predicted class of each image.
-    correct, cls_pred = sess.run([correct_prediction, y_pred_cls],
-                                 feed_dict=feed_dict_test)
-
-    # Negate the boolean array.
-    incorrect = (correct == False)
-
-    # Get the images from the test-set that have been
-    # incorrectly classified.
-    images = mnist.test.images[incorrect]
-
-    # Get the predicted classes for those images.
-    cls_pred = cls_pred[incorrect]
-
-    # Get the true classes for those images.
-    cls_true = mnist.test.cls[incorrect]
-
-    # Plot the first 9 images.
-    plot_images(images=images[0:9],
-                cls_true=cls_true[0:9],
-                cls_pred=cls_pred[0:9])
-
-
-def plot_images(images, cls_true, cls_pred=None):
-    assert len(images) == len(cls_true) == 9
-
-    # Create figure with 3x3 sub-plots.
-    fig, axes = plt.subplots(3, 3)
-    fig.subplots_adjust(hspace=0.3, wspace=0.3)
-
-    for i, ax in enumerate(axes.flat):
-        # Plot image.
-        ax.imshow(images[i].reshape((img_h, img_h)), cmap='binary')
-
-        # Show true and predicted classes.
-        if cls_pred is None:
-            xlabel = "True: {0}".format(cls_true[i])
-        else:
-            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
-
-        ax.set_xlabel(xlabel)
-
-        # Remove ticks from the plot.
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
-    plt.show()
---
+++ 2_MLP/ops.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+
+
+# weight and bais wrappers
+def weight_variable(name, shape):
+    """
+    Create a weight variable with appropriate initialization
+    :param name: weight name
+    :param shape: weight shape
+    :return: initialized weight variable
+    """
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W_' + name,
+                           dtype=tf.float32,
+                           shape=shape,
+                           initializer=initer)
+
+
+def bias_variable(name, shape):
+    """
+    Create a bias variable with appropriate initialization
+    :param name: bias variable name
+    :param shape: bias variable shape
+    :return: initialized bias variable
+    """
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b_' + name,
+                           dtype=tf.float32,
+                           initializer=initial)
+
+
+def fc_layer(x, num_units, name, use_relu=True):
+    """
+    Create a fully-connected layer
+    :param x: input from previous layer
+    :param num_units: number of hidden units in the fully-connected layer
+    :param name: layer name
+    :param use_relu: boolean to add ReLU non-linearity (or not)
+    :return: The output array
+    """
+    in_dim = x.get_shape()[1]
+    W = weight_variable(name, shape=[in_dim, num_units])
+    b = bias_variable(name, [num_units])
+    layer = tf.matmul(x, W)
+    layer += b
+    if use_relu:
+        layer = tf.nn.relu(layer)
+    return layer
