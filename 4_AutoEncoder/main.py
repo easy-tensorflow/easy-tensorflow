1cb0eb32886e4eb27062b3a2b63de067911646c8
Aryan <amobiny@hula-lt03.ee.e.uh.edu>
2018-02-20 11:26:11 -0600

    autoencoder .py files added

---
+++ 4_AutoEncoder/main.py
@@ -0,0 +1,124 @@
+# imports
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+
+from ops import fc_layer
+from utils import plot_max_active, plot_images
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(mnist.train.labels)))
+print("- Test-set:\t\t{}".format(len(mnist.test.labels)))
+print("- Validation-set:\t{}".format(len(mnist.validation.labels)))
+
+# hyper-parameters
+logs_path = "./logs/"  # path to the folder that we want to save the logs for Tensorboard
+learning_rate = 0.001  # The optimization learning rate
+epochs = 10  # Total number of training epochs
+batch_size = 100  # Training batch size
+display_freq = 100  # Frequency of displaying the training results
+
+# Network Parameters
+# We know that MNIST images are 28 pixels in each dimension.
+img_h = img_w = 28
+
+# Images are stored in one-dimensional arrays of this length.
+img_size_flat = img_h * img_w
+
+# number of units in the hidden layer
+h1 = 100
+
+# level of the noise in noisy data
+noise_level = 0.6
+
+# Create graph
+# Placeholders for inputs (x), outputs(y)
+with tf.variable_scope('Input'):
+    x_original = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X_original')
+    x_noisy = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X_noisy')
+
+
+fc1, W1 = fc_layer(x_noisy, h1, 'Hidden_layer', use_relu=True)
+out, W2 = fc_layer(fc1, img_size_flat, 'Output_layer', use_relu=False)
+
+x_active = W1 / tf.sqrt(tf.reduce_sum(tf.square(W1), axis=0))
+# [784, 100]
+
+# Define the loss function, optimizer, and accuracy
+with tf.variable_scope('Train'):
+    with tf.variable_scope('Loss'):
+        loss = tf.reduce_mean(tf.losses.mean_squared_error(x_original, out), name='loss')
+        tf.summary.scalar('loss', loss)
+    with tf.variable_scope('Optimizer'):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name='Adam-op').minimize(loss)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+merged = tf.summary.merge_all()
+
+# Launch the graph (session)
+with tf.Session() as sess:
+    sess.run(init)
+    train_writer = tf.summary.FileWriter(logs_path, sess.graph)
+    num_tr_iter = int(mnist.train.num_examples / batch_size)
+    global_step = 0
+    for epoch in range(epochs):
+        print('Training epoch: {}'.format(epoch + 1))
+        for iteration in range(num_tr_iter):
+            batch_x, _ = mnist.train.next_batch(batch_size)
+            batch_x_noisy = batch_x + noise_level * np.random.normal(loc=0.0, scale=1.0, size=batch_x.shape)
+
+            global_step += 1
+            # Run optimization op (backprop)
+            feed_dict_batch = {x_original: batch_x, x_noisy: batch_x_noisy}
+            _, summary_tr = sess.run([optimizer, merged], feed_dict=feed_dict_batch)
+            train_writer.add_summary(summary_tr, global_step)
+
+            if iteration % display_freq == 0:
+                # Calculate and display the batch loss and accuracy
+                loss_batch = sess.run(loss,
+                                      feed_dict=feed_dict_batch)
+                print("iter {0:3d}:\t Reconstruction loss={1:.3f}".
+                      format(iteration, loss_batch))
+
+        # Run validation after every epoch
+        x_valid_original  = mnist.validation.images
+        x_valid_noisy = x_valid_original + noise_level * np.random.normal(loc=0.0, scale=1.0, size=x_valid_original.shape)
+
+        feed_dict_valid = {x_original: x_valid_original, x_noisy: x_valid_noisy}
+        loss_valid = sess.run(loss, feed_dict=feed_dict_valid)
+        print('---------------------------------------------------------')
+        print("Epoch: {0}, validation loss: {1:.3f}".
+              format(epoch + 1, loss_valid))
+        print('---------------------------------------------------------')
+
+    # Test the network after training
+    # Make a noisy image
+    test_samples = 5
+    x_test = mnist.test.images[:test_samples]
+    x_test_noisy = x_test + noise_level * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+    # Reconstruct a clean image from noisy image
+    x_reconstruct = sess.run(out, feed_dict={x_noisy: x_test_noisy})
+    # Calculate the loss between reconstructed image and original image
+    loss_test = sess.run(loss, feed_dict={x_original: x_test, x_noisy: x_test_noisy})
+    print('---------------------------------------------------------')
+    print("Test loss of original image compared to reconstructed image : {0:.3f}".format(loss_test))
+    print('---------------------------------------------------------')
+
+    # Plot original image, noisy image and reconstructed image
+    plot_images(x_test, x_test_noisy, x_reconstruct)
+
+    # Plot the images that maximally activate the hidden units
+    plot_max_active(sess.run(x_active))
+
+
+
+
+
+
+
---
+++ 4_AutoEncoder/utils.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_images(original_images, noisy_images, reconstructed_images):
+    """
+    Create figure of original and reconstructed image.
+    :param original_image: original images to be plotted, (?, img_h*img_w)
+    :param noisy_image: original images to be plotted, (?, img_h*img_w)
+    :param reconstructed_image: reconstructed images to be plotted, (?, img_h*img_w)
+    """
+    num_images = original_images.shape[0]
+    fig, axes = plt.subplots(num_images, 3, figsize=(9, 9))
+    fig.subplots_adjust(hspace=.1, wspace=0)
+
+    img_h = img_w = np.sqrt(original_images.shape[-1]).astype(int)
+    for i, ax in enumerate(axes):
+        # Plot image.
+        ax[0].imshow(original_images[i].reshape((img_h, img_w)), cmap='gray')
+        ax[1].imshow(noisy_images[i].reshape((img_h, img_w)), cmap='gray')
+        ax[2].imshow(reconstructed_images[i].reshape((img_h, img_w)), cmap='gray')
+
+        # Remove ticks from the plot.
+        for sub_ax in ax:
+            sub_ax.set_xticks([])
+            sub_ax.set_yticks([])
+
+    for ax, col in zip(axes[0], ["Original Image", "Noisy Image", "Reconstructed Image"]):
+        ax.set_title(col)
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_max_active(x):
+    """
+    Plots the images that are maximally activating the hidden units
+    :param x: numpy array of size [input_dim, num_hidden_units]
+    """
+    fig, axes = plt.subplots(nrows=10, ncols=10)
+    fig.subplots_adjust(hspace=.1, wspace=0)
+    img_h = img_w = np.sqrt(x.shape[0]).astype(int)
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(x[:, i].reshape((img_h, img_w)), cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+    plt.show()
