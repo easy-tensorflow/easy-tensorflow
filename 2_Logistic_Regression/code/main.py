75fbe84aa14f2eacf8b94afd790de562be216dc0
Aryan <aryan.mobiny@gmail.com>
2018-05-04 00:35:57 -0500

    logistic regression 3

--- 2_Logistic_Regression/code/main.py
+++ 2_Logistic_Regression/code/main.py
@@ -9,3 +9,16 @@ print("Size of:")
 print("- Training-set:\t\t{}".format(len(y_train)))
 print("- Validation-set:\t{}".format(len(y_valid)))
 
+# Data Dimensions
+img_h = img_w = 28              # MNIST images are 28x28
+img_size_flat = img_h * img_w   # 28x28=784, the total number of pixels
+n_classes = 10                  # Number of classes, one class per digit
+
+# Create the graph for the linear model
+# Placeholders for inputs (x), outputs(y)
+x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X')
+y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
+
+
+
+
