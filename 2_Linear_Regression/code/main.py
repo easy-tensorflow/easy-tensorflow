098c0056d14c7129858c77ce4a88a608256ad18f
Aryan <aryan.mobiny@gmail.com>
2018-05-04 00:31:37 -0500

    logistic regression 1

---
+++ 2_Linear_Regression/code/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+from utils import *
+from ops import *
+
+# Load MNIST data
+x_train, y_train, x_valid, y_valid = load_data(mode='train')
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(y_train)))
+print("- Validation-set:\t{}".format(len(y_valid)))
+
