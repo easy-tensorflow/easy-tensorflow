f31fd006578e11f637cad39308a830b8bfedbf62
Aryan <aryan.mobiny@gmail.com>
2018-05-23 02:08:05 -0500

    linear 9

--- 2_Linear_Classifier/code/main.py
+++ 2_Linear_Classifier/code/main.py
@@ -3,16 +3,16 @@ import numpy as np
 from utils import *
 from ops import *
 
+# Data Dimensions
+img_h = img_w = 28  # MNIST images are 28x28
+img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
+n_classes = 10  # Number of classes, one class per digit
+
 # Load MNIST data
 x_train, y_train, x_valid, y_valid = load_data(mode='train')
 print("Size of:")
 print("- Training-set:\t\t{}".format(len(y_train)))
 print("- Validation-set:\t{}".format(len(y_valid)))
-
-# Data Dimensions
-img_h = img_w = 28  # MNIST images are 28x28
-img_size_flat = img_h * img_w  # 28x28=784, the total number of pixels
-n_classes = 10  # Number of classes, one class per digit
 
 # Hyper-parameters
 learning_rate = 0.001  # The optimization initial learning rate
@@ -21,7 +21,7 @@ batch_size = 100  # Training batch size
 display_freq = 100  # Frequency of displaying the training results
 
 # Create the graph for the linear model
-# Placeholders for inputs (x), outputs(y)
+# Placeholders for inputs (x) and outputs(y)
 x = tf.placeholder(tf.float32, shape=[None, img_size_flat], name='X')
 y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
 
