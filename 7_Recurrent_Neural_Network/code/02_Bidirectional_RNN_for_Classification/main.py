53cee88bb8acdd564008f49acf43208b857ed283
Aryan <aryan.mobiny@gmail.com>
2018-06-19 00:26:37 -0500

    Bidirectional RNN added

---
+++ 7_Recurrent_Neural_Network/code/02_Bidirectional_RNN_for_Classification/main.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+# Load MNIST data
+from ops import weight_variable, bias_variable, BiRNN
+from utils import load_data, randomize, get_next_batch
+
+x_train, y_train, x_valid, y_valid = load_data(mode='train')
+print("Size of:")
+print("- Training-set:\t\t{}".format(len(y_train)))
+print("- Validation-set:\t{}".format(len(y_valid)))
+
+# Hyper-parameters
+learning_rate = 0.001   # The optimization initial learning rate
+epochs = 10             # Total number of training epochs
+batch_size = 100        # Training batch size
+display_freq = 100      # Frequency of displaying the training results
+
+# Network Parameters
+num_input = 28          # MNIST data input (img shape: 28*28)
+timesteps = 28          # Timesteps
+num_hidden_units = 128  # Number of hidden units of the RNN
+n_classes = 10          # Number of classes, one class per digit
+
+
+# Create the graph for the linear model
+# Placeholders for inputs (x) and outputs(y)
+x = tf.placeholder(tf.float32, shape=[None, timesteps, num_input], name='X')
+y = tf.placeholder(tf.float32, shape=[None, n_classes], name='Y')
+
+
+# create weight matrix initialized randomely from N~(0, 0.01)
+# Hidden layer weights => 2*n_hidden because of forward + backward cells
+W = weight_variable(shape=[2*num_hidden_units, n_classes])
+
+# create bias vector initialized as zero
+b = bias_variable(shape=[n_classes])
+
+output_logits = BiRNN(x, W, b, timesteps, num_hidden_units)
+y_pred = tf.nn.softmax(output_logits)
+
+# Model predictions
+cls_prediction = tf.argmax(output_logits, axis=1, name='predictions')
+
+# Define the loss function, optimizer, and accuracy
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output_logits), name='loss')
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, name='Adam-op').minimize(loss)
+correct_prediction = tf.equal(tf.argmax(output_logits, 1), tf.argmax(y, 1), name='correct_pred')
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+
+# Creating the op for initializing all variables
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    global_step = 0
+    # Number of training iterations in each epoch
+    num_tr_iter = int(len(y_train) / batch_size)
+    for epoch in range(epochs):
+        print('Training epoch: {}'.format(epoch + 1))
+        x_train, y_train = randomize(x_train, y_train)
+        for iteration in range(num_tr_iter):
+            global_step += 1
+            start = iteration * batch_size
+            end = (iteration + 1) * batch_size
+            x_batch, y_batch = get_next_batch(x_train, y_train, start, end)
+            x_batch = x_batch.reshape((batch_size, timesteps, num_input))
+            # Run optimization op (backprop)
+            feed_dict_batch = {x: x_batch, y: y_batch}
+            sess.run(optimizer, feed_dict=feed_dict_batch)
+
+            if iteration % display_freq == 0:
+                # Calculate and display the batch loss and accuracy
+                loss_batch, acc_batch = sess.run([loss, accuracy],
+                                                 feed_dict=feed_dict_batch)
+
+                print("iter {0:3d}:\t Loss={1:.2f},\tTraining Accuracy={2:.01%}".
+                      format(iteration, loss_batch, acc_batch))
+
+        # Run validation after every epoch
+
+        feed_dict_valid = {x: x_valid[:1000].reshape((-1, timesteps, num_input)), y: y_valid[:1000]}
+        loss_valid, acc_valid = sess.run([loss, accuracy], feed_dict=feed_dict_valid)
+        print('---------------------------------------------------------')
+        print("Epoch: {0}, validation loss: {1:.2f}, validation accuracy: {2:.01%}".
+              format(epoch + 1, loss_valid, acc_valid))
+        print('---------------------------------------------------------')
+
+    # Test the network after training
+    # Accuracy
+    x_test, y_test = load_data(mode='test')
+    feed_dict_test = {x: x_test[:1000].reshape((-1, timesteps, num_input)), y: y_test[:1000]}
+    loss_test, acc_test = sess.run([loss, accuracy], feed_dict=feed_dict_test)
+    print('---------------------------------------------------------')
+    print("Test loss: {0:.2f}, test accuracy: {1:.01%}".format(loss_test, acc_test))
+    print('---------------------------------------------------------')
---
+++ 7_Recurrent_Neural_Network/code/02_Bidirectional_RNN_for_Classification/ops.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+
+def weight_variable(shape):
+    """
+    Create a weight variable with appropriate initialization
+    :param name: weight name
+    :param shape: weight shape
+    :return: initialized weight variable
+    """
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W',
+                           dtype=tf.float32,
+                           shape=shape,
+                           initializer=initer)
+
+
+def bias_variable(shape):
+    """
+    Create a bias variable with appropriate initialization
+    :param name: bias variable name
+    :param shape: bias variable shape
+    :return: initialized bias variable
+    """
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b',
+                           dtype=tf.float32,
+                           initializer=initial)
+
+
+def BiRNN(x, weights, biases, timesteps, num_hidden):
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, timesteps, n_input)
+    # Required shape: 'timesteps' tensors list of shape (batch_size, num_input)
+
+    # Unstack to get a list of 'timesteps' tensors of shape (batch_size, num_input)
+    x = tf.unstack(x, timesteps, 1)
+
+    # Define lstm cells with tensorflow
+    # Forward direction cell
+    lstm_fw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+    # Backward direction cell
+    lstm_bw_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+
+    # Get BiRNN cell output
+    outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
+                                                 dtype=tf.float32)
+
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights) + biases
