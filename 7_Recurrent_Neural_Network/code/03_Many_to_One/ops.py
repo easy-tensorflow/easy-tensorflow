ed2047172416ce421031ceb4ddc2f05c107857d5
Aryan Mobiny <amobiny@uh.edu>
2018-07-02 18:27:25 -0500

    rnn tutorials resorted

--- 7_Recurrent_Neural_Network/code/03_Many_to_One/ops.py
+++ 7_Recurrent_Neural_Network/code/03_Many_to_One/ops.py
@@ -29,7 +29,7 @@ def bias_variable(shape):
                            initializer=initial)
 
 
-def Dynamic_LSTM(x, weights, biases, num_hidden):
+def LSTM(x, weights, biases, num_hidden):
     """
     :param x: inputs of size [T, batch_size, input_size]
     :param weights: matrix of fully-connected output layer weights
@@ -44,26 +44,26 @@ def Dynamic_LSTM(x, weights, biases, num_hidden):
     return out
 
 
-def Static_LSTM(x, weights, biases, timesteps, num_hidden):
-
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, timesteps, n_input)
-    # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
-
-    # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
-    num_examples = tf.shape(x)[0]
-    x = tf.unstack(x, timesteps, 1)
-
-    # Define a rnn cell with tensorflow
-    cell = tf.nn.rnn_cell.LSTMCell(num_hidden)
-    w_repeated = tf.tile(tf.expand_dims(weights, 0), [num_examples, 1, 1])
-    # Get lstm cell output
-    # If no initial_state is provided, dtype must be specified
-    # If no initial cell state is provided, they will be initialized to zero
-    outputs_series, current_state = tf.nn.static_rnn(cell, x, dtype=tf.float32)
-    outputs = tf.stack(outputs_series, axis=1)
-    out = tf.matmul(outputs, w_repeated) + biases
-    out = tf.squeeze(out)
-
-    # Linear activation, using rnn inner loop last output
-    return out
+# def Static_LSTM(x, weights, biases, timesteps, num_hidden):
+#
+#     # Prepare data shape to match `rnn` function requirements
+#     # Current data input shape: (batch_size, timesteps, n_input)
+#     # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
+#
+#     # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
+#     num_examples = tf.shape(x)[0]
+#     x = tf.unstack(x, timesteps, 1)
+#
+#     # Define a rnn cell with tensorflow
+#     cell = tf.nn.rnn_cell.LSTMCell(num_hidden)
+#     w_repeated = tf.tile(tf.expand_dims(weights, 0), [num_examples, 1, 1])
+#     # Get lstm cell output
+#     # If no initial_state is provided, dtype must be specified
+#     # If no initial cell state is provided, they will be initialized to zero
+#     outputs_series, current_state = tf.nn.static_rnn(cell, x, dtype=tf.float32)
+#     outputs = tf.stack(outputs_series, axis=1)
+#     out = tf.matmul(outputs, w_repeated) + biases
+#     out = tf.squeeze(out)
+#
+#     # Linear activation, using rnn inner loop last output
+#     return out
