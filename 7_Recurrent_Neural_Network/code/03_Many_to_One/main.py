f9bf4ac21fa5553d16afc6837f47ce25c24ace15
Aryan Mobiny <amobiny@uh.edu>
2018-07-02 19:08:34 -0500

    rnn many_to_one 1

--- 7_Recurrent_Neural_Network/code/03_Many_to_One/main.py
+++ 7_Recurrent_Neural_Network/code/03_Many_to_One/main.py
@@ -2,14 +2,13 @@ import tensorflow as tf
 import numpy as np
 from ops import *
 
-
 input_dim = 1
 max_time = 4
 num_hidden_units = 10
 out_dim = 1
 
 x = tf.placeholder(tf.float32, [None, max_time, input_dim])
-y = tf.placeholder(tf.float32, [None])
+y = tf.placeholder(tf.float32, [None, 1])
 
 # create weight matrix initialized randomely from N~(0, 0.01)
 W = weight_variable(shape=[num_hidden_units, out_dim])
@@ -17,8 +16,7 @@ W = weight_variable(shape=[num_hidden_units, out_dim])
 # create bias vector initialized as zero
 b = bias_variable(shape=[out_dim])
 
-# pred_out = Dynamic_LSTM(x, W, b, num_hidden_units)
-pred_out = Static_LSTM(x, W, b, max_time, num_hidden_units)
+pred_out = LSTM(x, W, b, num_hidden_units)
 
 cost = tf.reduce_mean(tf.square(pred_out - y))
 train_op = tf.train.AdamOptimizer().minimize(cost)
@@ -27,18 +25,18 @@ train_op = tf.train.AdamOptimizer().minimize(cost)
 init = tf.global_variables_initializer()
 
 x_train = np.array([[[1], [2], [5], [6]],
-           [[5], [7], [7], [8]],
-           [[3], [4], [5], [9]]])
-y_train = np.array([[1, 3, 8, 14],
-           [5, 12, 19, 27],
-           [3, 7, 12, 21]])
+                    [[5], [7], [4], [8]],
+                    [[2], [5], [1], [3]],
+                    [[9], [0], [3], [1]],
+                    [[3], [1], [0], [6]],
+                    [[6], [2], [4], [7]],
+                    [[3], [4], [5], [3]]])
+y_train = x_train[:, -1]
 
 x_test = np.array([[[1], [2], [3], [4]],
-          [[4], [5], [6], [7]]])
+                   [[4], [5], [6], [7]]])
 
-y_test = np.array([[[1], [3], [6], [10]],
-            [[4], [9], [15], [22]]])
-
+y_test = x_test[:, -1]
 
 with tf.Session() as sess:
     sess.run(init)
@@ -47,7 +45,7 @@ with tf.Session() as sess:
         if i % 1000 == 0:
             print('Step {}, MSE={}'.format(i, mse))
     # Test
-        y_pred = sess.run(pred_out, feed_dict={x: x_test})
+    y_pred = sess.run(pred_out, feed_dict={x: x_test})
 
     for i, x in enumerate(x_test):
         print("When the input is {}".format(x))
